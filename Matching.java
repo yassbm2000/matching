e36db3ab28c70556c7780d53fa917dfa4d20db42
AymanDm <96471055+AymanDm@users.noreply.github.com>
2021-12-26 13:48:46 +0100

    Update Matching.java

--- Matching.java
+++ Matching.java
@@ -3,4 +3,19 @@ public class Matching {
 	HashSet<School> schools;
 	HashSet<Student> students;
 	HashMap<School,HashSet<Student>> match;
+	
+	
+	public Matching(HashSet<School> schools, HashSet<Student> students, HashMap<School,HashSet<Student>> match ){
+		this.schools = schools; this.students = students; this.match = match;
+	}
+	
+	public void basiccasematch() {
+	LinkedList<Student> temp = students; // how do I turn a hashset into a list 
+	while ( temp.isNotEmpty()) {
+		Student cur =temp.pop();
+		School sco = cur.nextChoice();
+		if ( cur.betterThanNthg(sco) && sco.students.length() < sco.capacity) {sco.students.add(cur);}
+		this.match.put(sco,cur);
+		
+	}
 }
