c09e560d01c0d8536c6a7377dba71bbbaf351e56
AymanDm <96471055+AymanDm@users.noreply.github.com>
2021-12-27 14:26:50 +0100

    Update Matching.java

--- Matching.java
+++ Matching.java
@@ -19,8 +19,9 @@ public class Matching {
 				for(Student s : sco.admitted){
 					if(sco.compareStudents(cur,s)>0) {
 						sco.admitted.remove(s);
-						temp.add(s); // add the fired student to the waiting pile
+						temp.add(s); // add the "fired" student to the waiting pile
 						sco.admitted.add(cur);
+						cur.assignedSchool = sco;
 						break;
 					}
 				}
@@ -31,12 +32,40 @@ public class Matching {
 	}
 	
 	public void groupQuotaMatch() {
+		this.match = new HashMap<School,HashSet<Student>>; //reset the match 
 		LinkedList<Student> temp = new LinkedList<Student>(students); 
 		for ( Student s: students){ s.resetPreference();} // reset preferences ( may have been modified from last matching ) 
 		while ( !temp.isEmpty()) {
 			Student cur =temp.pop();
 			School sco = cur.nextChoice();
-			if (sco.
+			int g = cur.group;
+			if (sco.admittedInGroup(g).size() < sco.quotas.get(g) && cur.betterThanNthg(sco) ){
+				if ( sco.admitted.size() < sco.capacity ) {
+				sco.admitted.add(cur); cur.assignedSchool = sco; ;} // add to schools a field " assigned from group" 
+		} else {
+				for ( Student s: sco.admitted){ 
+				if (sco.compareStudents(cur,s) > 0 ){
+					sco.admitted.remove(s);temp.add(s); 
+					sco.admitted.add(cur); sco.admittedInGroup(g).add(cur);
+					cur.assignedSchool = sco;
+					break;
+				}
+			} 
+			
+		       }
+			else  {
+				for ( Student s: sco.admittedInGroup(g)){
+					if ( sco.compareStudent(cur,s) > 0 ) { 
+					sco.admitted.remove(s); temp.add(s);
+					sco.admitted.add(cur); sco.admittedInGroup(g).add(cur);
+					sco.admittedInGroup(g).remove(s); 
+					break;
+					}
+				}
+			}
+			
+		}
 		
+	}
 		
 }
