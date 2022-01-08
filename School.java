32862ce0edc9b8131050abbc091fd6571ee310b3
yassbm2000 <91199937+yassbm2000@users.noreply.github.com>
2022-01-08 18:48:02 +0100

    Update School.java

--- School.java
+++ School.java
@@ -21,4 +21,42 @@ public class School {
 	public int compareStudents(Student s1,Student s2) {//returns -1 if s2 is preferred by this school than s1 and 1 otherwise
 		return Integer.compare(this.preferences.indexOf(s2), this.preferences.indexOf(s1));
 	}
+	public int groupSize(int g){//returns the number of students belonging to group g
+		int count = 0;
+		for(Student e : this.preferences){
+			if(e.group == g) {count++;}
+		}
+		return count;
+	}
+	public void truncate() {
+		boolean a = false;
+		for(Student e : this.preferences) {
+			if(e==null) {
+				a = true;
+			}
+			if(a)
+				this.preferences.remove(e);
+		}
+	}
+	
+	//fair rankings
+	public void fairRanking(){
+		//the goal is to apply the 4/5 rules to every school's preferences
+		this.truncate();
+		HashMap<Integer,Integer> groupCount = new HashMap<Integer,Integer>();
+		for(Student e : this.preferences){
+			int ge = e.group;
+			if(groupCount.get(ge)==null)
+				groupCount.put(ge,0);
+			else {
+				int c = groupCount.get(ge);
+				double grpFraction = (((double) this.groupSize(ge))/this.preferences.size());
+				if( (c+1)/ ((double) this.capacity) < 6*grpFraction/5 )
+					groupCount.put(ge, c+1);
+				else 
+					this.preferences.remove(e);	
+			}
+		}
+	}
+			
 }
