52f962ce940f32f339df6de5cbfe848cdb55d04a
AymanDm <96471055+AymanDm@users.noreply.github.com>
2022-01-31 21:06:40 +0100

    Update Groupquotas.java

--- Groupquotas.java
+++ Groupquotas.java
@@ -8,13 +8,15 @@ public class Groupquotas extends Constraint {
 	boolean constraint(School s, HashSet<Student> h) {
 		boolean cap = h.size() <= s.capacity; //capacity constraint
 		boolean quota = true;
-		for (Integer g=0; g< s.quotas.size(); g++) {
+		for (Student e: s.preferences) {
+			int g = e.group;
 			if (s.admittedInGroup.get(g)!=null && s.admittedInGroup.get(g).size()>s.quotas.get(g)) {
-				System.out.println(s.admittedInGroup.get(g).size()); quota =false; break;
+				 quota =false; break;
 			}
 			
+			
 		}
-		return cap && quota;
+		return     cap && quota;
 
 		
 	}
@@ -23,9 +25,10 @@ public class Groupquotas extends Constraint {
 		Groupquotas gq = new Groupquotas();
 		Instance2 t4 = new Instance2(20);
 		Instance3 t5 = new Instance3(20);
-		t4.match.fixedPoint(gq); t5.match.fixedPoint(gq);
-		System.out.println(t4.match.toString());
+		//t4.match.fixedPoint(gq);
+		t5.match.fixedPoint(gq);
 		System.out.println(t5.match.toString());
+		//System.out.println(t4.match.toString());
 
 	}
 
